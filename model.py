9f7e56191a45b8e61e0401150becaa831bc1cc60
Davyd Bandeira de melo <davydmelo@gmail.com>
2017-02-23 16:40:18 -0300

    - Adição de modelos User, Vendor, Material

--- model.py
+++ model.py
@@ -1,8 +1,59 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
 Base = declarative_base()
+
+class User(Base):
+
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    name = Column(String)
+    reg_number = Column(Integer)
+    is_admin = Column(Boolean)
+
+    def is_username_valid(self):
+        return len(self.username) >= 6
+
+class Vendor(Base):
+
+    __tablename__ = 'vendor'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    materials = relationship("Material", back_populates="vendor")
+
+class Material(Base):
+
+    __tablename__ = 'material'
+
+    id = Column(Integer, primary_key=True)
+    code = Column(Integer)
+    name = Column(String)
+    model = Column(String)
+
+    vendor_id = Column(Integer, ForeignKey('vendor.id'))
+    vendor = relationship("Vendor", back_populates="materials")
+
+    def is_username_valid(self):
+        return len(self.username) >= 6
+
+class Reservation(Base):
+
+    __tablename__ = 'reservation'
+
+    id = Column(Integer, primary_key=True)
+    begin = Column(DateTime)
+    end = Column(DateTime)
+
+    workstation_id = Column(Integer, ForeignKey('workstation.id'))
+    workstation = relationship("Workstation", back_populates="reservations")
+
+    def serialize(self):
+        return {"id": self.id, "begin": self.begin, "end": self.end}
+
 
 class Workstation(Base):
 
@@ -10,11 +61,14 @@ class Workstation(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
+
     laboratory_id = Column(Integer, ForeignKey('laboratory.id'))
     laboratory = relationship("Laboratory", back_populates="workstations")
 
+    reservations = relationship("Reservation", order_by=Reservation.id, back_populates="workstation")
+
     def serialize(self):
-        return {'id': self.id, 'name': self.name }
+        return {'id': self.id, 'name': self.name, "reservations" : [r.serialize() for r in self.reservations]}
 
 
 class Laboratory(Base):
@@ -28,7 +82,7 @@ class Laboratory(Base):
     workstations = relationship("Workstation", order_by = Workstation.id, back_populates = "laboratory")
 
     def serialize(self):
-        return {'id': self.id, 'name': self.name, 'description': self.description}
+        return {'id': self.id, 'name': self.name, 'description': self.description, "workstations" : [w.serialize() for w in self.workstations]}
 
 def getBase():
     return Base
